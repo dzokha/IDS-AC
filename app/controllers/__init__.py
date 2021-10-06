c5ba89ffed1ccc0b550ce402da2af45804f779c6
Van Kha Nguyen <dzokha1010@gmail.com>
2021-10-07 03:38:57 +0700

    alert complete

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -4,19 +4,7 @@ from flask import Blueprint, current_app
 from app.controllers.alert_controller import index as alert_index
 from app.controllers.alert_controller import alert_to_database
 from app.controllers.alert_controller import alert_report
-
-from app.controllers.snort_controller import index as snort_index
-from app.controllers.snort_controller import snort_to_csv
-from app.controllers.snort_controller import featurefortraining
-from app.controllers.snort_controller import exe_featurefortraining
-
-from app.controllers.abnormal_controller import training
-from app.controllers.abnormal_controller import execute_training
-from app.controllers.abnormal_controller import monitoring
-from app.controllers.abnormal_controller import execute_monitoring
-from app.controllers.abnormal_controller import info_alert
-from app.controllers.abnormal_controller import testing
-from app.controllers.abnormal_controller import execute_testing
+from app.controllers.alert_controller import alert_fix_country
 
 
 template_dir = os.path.abspath('app/views')
@@ -25,19 +13,4 @@ alert_blueprints = Blueprint('alert', __name__, template_folder=template_dir)
 alert_blueprints.add_url_rule('/', view_func=alert_index, methods=['GET'])
 alert_blueprints.add_url_rule('/execute', endpoint='execute', view_func=alert_to_database, methods=['GET', 'POST'])
 alert_blueprints.add_url_rule('/report', endpoint='report', view_func=alert_report, methods=['GET'])
-
-snort_blueprints = Blueprint('snort', __name__, template_folder=template_dir)
-snort_blueprints.add_url_rule('/', view_func=snort_index, methods=['GET'])
-snort_blueprints.add_url_rule('/execute', endpoint='execute', view_func=snort_to_csv, methods=['GET', 'POST'])
-snort_blueprints.add_url_rule('/featurefortraining', view_func=featurefortraining, methods=['GET'])
-snort_blueprints.add_url_rule('/format', endpoint='format', view_func=exe_featurefortraining, methods=['GET', 'POST'])
-
-abnormal_blueprints = Blueprint('abnormal', __name__, template_folder=template_dir)
-abnormal_blueprints.add_url_rule('/training', endpoint='training', view_func=training, methods=['GET','POST'])
-abnormal_blueprints.add_url_rule('/execute-training', endpoint='execute-training', view_func=execute_training, methods=['GET', 'POST'])
-abnormal_blueprints.add_url_rule('/monitoring', endpoint='monitoring', view_func=monitoring, methods=['GET','POST'])
-abnormal_blueprints.add_url_rule('/execute-monitoring', endpoint='execute-monitoring', view_func=execute_monitoring, methods=['GET', 'POST'])
-abnormal_blueprints.add_url_rule('/info-alert', endpoint='info-alert', view_func=info_alert, methods=['GET'])
-abnormal_blueprints.add_url_rule('/testing', endpoint='testing', view_func=testing, methods=['GET','POST'])
-abnormal_blueprints.add_url_rule('/execute-testing', endpoint='execute-testing', view_func=execute_testing, methods=['GET', 'POST'])
-
+alert_blueprints.add_url_rule('/fix-country', endpoint='fix-country', view_func=alert_fix_country, methods=['GET'])
--- app/controllers/alert_controller.py
+++ app/controllers/alert_controller.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, sys
 import json
 if sys.version_info[0] < 3:
 	from urllib2 import urlopen
@@ -8,10 +7,11 @@ else:
 from flask import current_app, flash, request, jsonify, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime
-from sqlalchemy import func
-from sqlalchemy import exc
 from app.models import db
 from app.models.alert import Alert
+from sqlalchemy import func
+from sqlalchemy import exc
+import sqlalchemy.pool as pool
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
@@ -24,7 +24,8 @@ def index():
 	result = db.session.query(Alert.priority, Alert.classification, Alert.msg, func.count(Alert.msg)).group_by(Alert.priority, Alert.classification,Alert.msg).all()
 	return render_template("alert/index.html", alert=result)
 
-#@user_blueprints.route('/alert', method="POST")
+
+#@user_blueprints.route('/execute', method="POST")
 def alert_to_database():
 	""" run csvalert"""
 	year_alert = current_app.config["YEAR_ALERT"]
@@ -33,7 +34,7 @@ def alert_to_database():
 	# Create a directory in a known location to save files to.
 	uploads_dir = os.path.join(current_app.config["UPLOAD_FOLDER"], 'alert')
 	os.makedirs(uploads_dir, exist_ok = True)
-	
+
 	if request.method == 'POST':
 		multifile = request.files.getlist('path[]')
 		if multifile:
@@ -43,6 +44,7 @@ def alert_to_database():
 			for file in multifile:
 				file_name = secure_filename(file.filename)
 				file_log = uploads_dir + '/'+file_name
+
 				with open(file_log) as multi_line:
 					four_line = error_line = 0
 					for line in multi_line:
@@ -67,6 +69,9 @@ def alert_to_database():
 											record = Alert(gid_sid_rev, msg, classification, priority, timestamp, date_time,src_ip,src_port,dst_ip,dst_port,meta_data)
 											db.session.add(record)
 											db.session.commit()
+										# else:
+										# 	print('Double key')
+										# 	break
 							else:
 								str_line = line.strip()
 								if four_line == 1:
@@ -102,9 +107,11 @@ def alert_to_database():
 									dst_port = dst[1]
 								if four_line > 3:
 									meta_data = meta_data + " " + str_line
+
 						except Exception as ex:
 							# deplay = input('Enter for continue.\n')
 							print(ex)
+
 			flash(str(multifile),'success')
 			return redirect(url_for('alert.index', filename=file_name))
 		else:
@@ -112,7 +119,34 @@ def alert_to_database():
 			return redirect(request.url)
 	return redirect(url_for('alert.index'))
 
-#@user_blueprints.route('/alert', method="POST")
+
+#@user_blueprints.route('/report, method="GET")
 def alert_report():
 	result = db.session.query(Alert.loc, Alert.country).where(Alert.country != None).distinct().all()
-	return render_template("alert/report.html", alert=result)
+	country_null = db.session.query(func.count(Alert.src_ip.distinct())).filter(Alert.country.is_(None)).first()
+	country = country_null[0] - 1
+	return render_template("alert/report.html", alert=result, country=country)
+
+#@user_blueprints.route('/fix-country, method="GET")
+def alert_fix_country():
+	try:
+		ip_filter = current_app.config["IP_FILTER"]
+		ip_honeynet = current_app.config["IP_HONEYNET"]
+		result = db.session.query(Alert.dst_ip).where(Alert.country == None).distinct().all()
+		for row in result:
+			ip = row[0]
+			if ip != ip_honeynet and ip not in ip_filter:
+				ipmap = 'http://ipinfo.io/' + ip +'/json'
+				response = urlopen(ipmap).read()
+				json_data = json.loads(response)
+				loc = json_data.get('loc')
+				country = json_data.get('country')
+
+				db.session.query(Alert).filter((Alert.dst_ip.like(ip)) | (Alert.src_ip.like(ip))).update({Alert.loc:loc, Alert.country:country}, synchronize_session = False)
+				db.session.commit()
+		flash('Fix country success','success')
+	except:
+		flash('Free usage of ipinfo.io API is limited to 50,000 API requests per month','success')
+		pass
+	
+	return redirect(url_for('alert.report'))
